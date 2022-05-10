b7ea084551cb70549607da7df260d36f937221ee
Dima Shirkov <dimashirkov@list.ru>
2022-05-10 22:17:49 +0300

    dak1 draft v5

--- task.js
+++ task.js
@@ -1,23 +1,18 @@
-// let hole = document.getElementsByClassName('hole');
-// let holeHasMole = document.getElementsByClassName('hole_has-mole');
-// //let holeId = document.getElementById(`hole${index}`);
-let dead = document.getElementById('dead');
+const dead = document.getElementById('dead');
 dead.textContent = 0;
-let blunder = document.getElementById('lost');
+const blunder = document.getElementById('lost');
 blunder.textContent = 0;
 
+function getHole(index) {
+    const desiredClass = document.getElementById(`hole${index}`);
+    return desiredClass;
+}
 
 for (index = 1; index <= 9; index++) {
-    
-    function getHole(index) {
-        const desiredClass = document.getElementById(`hole${index}`);
-        return desiredClass;
-    }
-
     let hole = getHole(index);
 
     hole.onclick = function () {
-        if (hole.className.includes('hole_has-mole') === true) {
+        if (hole.className.includes('hole_has-mole')) {
             dead.textContent++;
         } else {blunder.textContent++;}
 
@@ -32,21 +27,3 @@ for (index = 1; index <= 9; index++) {
         }
     }
 }
-
-
-
-// if (activateHole = index =>
-//     getHole( index ).className = 'hole hole_has-mole') {
-//         ++dead
-// } else if (deactivateHole = index =>
-//         getHole( index ).className = 'hole') {++blunder}
-
-// for (i = 0; i < hole.length; i++) {
-//     hole[i].onclick = function () {
-//         if (hole.className === 'hole hole_has-mole') {
-//             ++dead
-//         } else {++blunder}
-//     }
-// } 
-
-
